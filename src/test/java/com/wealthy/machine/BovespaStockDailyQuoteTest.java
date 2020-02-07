b8e4115100fbfe80695271a0f34edc3de69da20d
luiza <luizaugusto3b@gmail.com>
2020-02-07 21:14:35 +0100

    Adding the lombok library

--- src/test/java/com/wealthy/machine/BovespaStockDailyQuoteTest.java
+++ src/test/java/com/wealthy/machine/BovespaStockDailyQuoteTest.java
@@ -8,7 +8,6 @@ import static org.junit.Assert.assertEquals;
 
 public class BovespaStockDailyQuoteTest {
 
-
     @Rule
     public ExpectedException expectedEx = ExpectedException.none();
 
@@ -16,10 +15,14 @@ public class BovespaStockDailyQuoteTest {
         return "012003021202VALE3       010VALE R DOCE ON           R$  000000001050100000000105010000000010250000000001036800000000103210000000010321000000001043800142000000000000069500000000000720641400000000000000009999123100000010000000000000BRVALEACNOR0159";
     }
 
+    private String getObjectTested(){
+        return "BovespaStockDailyQuote(tradingDay=Wed Feb 12 00:00:00 CET 2003, stockCode=VALE3, company=VALE R DOCE, openPrice=105.01, closePrice=103.21, minPrice=102.5, maxPrice=105.01, avgPrice=103.68, volume=7206414.0)";
+    }
+
     @Test
     public void createDataInfo() {
         BovespaStockDailyQuote bovespaStockDailyQuote = new BovespaStockDailyQuote(getLine());
-        assertEquals("BovespaStockDailyQuote(tradingDay=Wed Feb 12 00:00:00 CET 2003, stockCode=VALE3, company=VALE R DOCE, openPrice=105.01, closePrice=103.21, minPrice=102.5, maxPrice=105.01, avgPrice=103.68, volume=7206414.0)", bovespaStockDailyQuote.toString());
+        assertEquals(getObjectTested(), bovespaStockDailyQuote.toString());
     }
 
     @Test

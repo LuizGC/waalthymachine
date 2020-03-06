89bfc44d1f914fbd302b8764f4125a300b5f1996
Luiz_Costa <luizaugusto3b@gmail.com>
2020-03-06 12:47:02 +0100

    Using java NIO to download file and processing the content

--- src/main/java/com/wealthy/machine/reader/BovespaDataReader.java
+++ src/main/java/com/wealthy/machine/reader/BovespaDataReader.java
@@ -3,16 +3,14 @@ package com.wealthy.machine.reader;
 import com.wealthy.machine.quote.BovespaStockDailyQuote;
 import lombok.Cleanup;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.net.URL;
+import java.nio.ByteBuffer;
 import java.nio.channels.Channels;
-import java.nio.channels.FileChannel;
 import java.nio.channels.ReadableByteChannel;
+import java.nio.channels.WritableByteChannel;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.stream.Stream;
 import java.util.zip.ZipInputStream;
@@ -22,17 +20,13 @@ public class BovespaDataReader implements DataReader {
     public Stream<BovespaStockDailyQuote> read(String zipFileUrl) {
         try {
             URL url = new URL(zipFileUrl);
-            Path tempFile = Files.createTempFile("temp_quotes", ".zip");
-            @Cleanup ReadableByteChannel readableByteChannel = Channels.newChannel(url.openStream());
-            @Cleanup FileOutputStream fileOutputStream = new FileOutputStream(tempFile.toFile());
-            @Cleanup FileChannel fileChannel = fileOutputStream.getChannel();
-            @Cleanup ZipInputStream zipStream =  new ZipInputStream(new FileInputStream(tempFile.toFile()));
-            fileChannel.transferFrom(readableByteChannel, 0, Long.MAX_VALUE);
+            @Cleanup ZipInputStream zipStream =  new ZipInputStream(url.openStream());
+            @Cleanup ReadableByteChannel readableByteChannel = Channels.newChannel(zipStream);
             if (zipStream.getNextEntry() == null) {
                 throw new Exception("Zip is not valid!");
             } else {
-                Stream<BovespaStockDailyQuote> stream = readEntry(zipStream);
-                tempFile.toFile().delete();
+                Stream<BovespaStockDailyQuote> stream = readEntry(readableByteChannel);
+
                 return stream;
             }
         } catch (Exception e) {
@@ -40,15 +34,18 @@ public class BovespaDataReader implements DataReader {
         }
     }
 
-    private Stream<BovespaStockDailyQuote> readEntry(ZipInputStream zipStream) throws IOException {
-        byte [] buffer = new byte[16384];
-        int totalRead;
-        String stringBuffer = "";
-        while ((totalRead = zipStream.read(buffer)) > 0) {
-            stringBuffer += new String(buffer, StandardCharsets.UTF_8).substring(0, totalRead);
+    private Stream<BovespaStockDailyQuote> readEntry(ReadableByteChannel readableByteChannel) throws IOException {
+        @Cleanup ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        @Cleanup WritableByteChannel out = Channels.newChannel(bos);
+        ByteBuffer bb = ByteBuffer.allocate(16384);
+        while ((readableByteChannel.read(bb)) > 0) {
+            bb.flip();
+            out.write(bb);
+            bb.clear();
         }
+        String content = new String(bos.toByteArray(), StandardCharsets.UTF_8);
         return Arrays
-                .stream(stringBuffer.split("\\r?\\n"))
+                .stream(content.split("\\r?\\n"))
                 .map(line -> line.trim())
                 .filter(line -> line.length() == 245)
                 .map(BovespaStockDailyQuote::new);

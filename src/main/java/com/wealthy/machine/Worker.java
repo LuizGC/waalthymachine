bebf65ce976f17968d8bfd80095ca75248605f1c
luiza <luizaugusto3b@gmail.com>
2020-04-27 10:45:02 +0200

    - Hiding worker methods and constructors

--- src/main/java/com/wealthy/machine/Worker.java
+++ src/main/java/com/wealthy/machine/Worker.java
@@ -15,24 +15,37 @@ import java.nio.file.Paths;
 
 public class Worker {
 
-	public static void main(String... args) throws IOException {
-		var path = args.length == 0 ? Files.createTempDirectory("storage_folder") : Path.of(args[0]);
-		var storageFolder = path.toFile();
-		var logFolder = new File(storageFolder, "log");
-		System.setProperty("LOG_FILE", Paths.get(logFolder.toString(), "worker").toString());
-		Logger logger = LoggerFactory.getLogger(Worker.class);
+	private final Logger logger;
+	private final File storageFolder;
+	private final File logFile;
+
+	private Worker(Path path) {
+		this.storageFolder = path.toFile();
+		this.logFile = new File(storageFolder, "wealthy");
+		System.setProperty("LOG_FILE", this.logFile.toString());
+		this.logger = LoggerFactory.getLogger(this.getClass());
 		logger.info("Worker started");
-		run(new BovespaDataReader(), new BovespaStockQuoteDataAccessLayer(storageFolder));
+		try {
+			run(new BovespaDataReader(), new BovespaStockQuoteDataAccessLayer(storageFolder));
+		} catch (Exception e) {
+			logger.error("Worker doen't finish", e);
+		}
 		logger.info("storageFolder={}", storageFolder);
-		logger.info("logFolder={}", logFolder);
+		logger.info("logFolder={}", logFile);
 		logger.info("Worker ended");
 	}
 
-	public static void run (DataReader dataReader, StockQuoteDataAccessLayer stockShareDataAccessLayer) {
+	private void run (DataReader dataReader, StockQuoteDataAccessLayer stockShareDataAccessLayer) {
 		stockShareDataAccessLayer
 				.listUnsavedPaths()
 				.parallelStream()
 				.map(dataReader::read)
 				.forEach(stockShareDataAccessLayer::save);
 	}
+
+	public static void main(String... args) throws IOException {
+		var path = args.length == 0 ? Files.createTempDirectory("storage_folder") : Path.of(args[0]);
+		new Worker(path);
+	}
+
 }

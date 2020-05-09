9dd99cf341c0b3e9ded70716a17b24bb07736689
luiza <luizaugusto3b@gmail.com>
2020-05-09 18:47:24 +0200

    - testing github secrets

--- src/test/java/com/wealthy/machine/core/ConfigTest.java
+++ src/test/java/com/wealthy/machine/core/ConfigTest.java
@@ -1,10 +1,14 @@
 package com.wealthy.machine.core;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ConfigTest {
 
+	@Test
 	public void getSecretsValueFromGitHub_GetValue_ShouldBeEquals() {
+		System.out.println(System.getenv("DOES_IT_WORKS"));
 		assertEquals("works!!!", System.getenv("DOES_IT_WORKS"));
 	}
 

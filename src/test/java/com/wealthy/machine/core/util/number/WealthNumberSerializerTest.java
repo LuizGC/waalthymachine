db3b7a5d2a59385140ef635859c6869310ca5fd4
luiza <luizaugusto3b@gmail.com>
2020-05-22 18:34:14 +0200

    - add WealthNumberSerializerTest

---
+++ src/test/java/com/wealthy/machine/core/util/number/WealthNumberSerializerTest.java
@@ -0,0 +1,26 @@
+package com.wealthy.machine.core.util.number;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+
+class WealthNumberSerializerTest {
+
+	@Test
+	void serialize() throws IOException {
+		var serializer = new WealthNumberSerializer();
+		var number = mock(WealthNumber.class);
+		var jsonGenerator = mock(JsonGenerator.class);
+		var serializerProvider = mock(SerializerProvider.class);
+		serializer.serialize(number, jsonGenerator, serializerProvider);
+		when(number.toString()).thenReturn("42");
+		verify(jsonGenerator, times(1)).writeNumber(anyString());
+	}
+
+}

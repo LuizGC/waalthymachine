a250000dda0e045de6d3aec09e42003218170794
luiza <luizaugusto3b@gmail.com>
2020-05-31 17:29:38 +0200

    - optimizing imports

--- src/main/java/com/wealthy/machine/bovespa/quote/BovespaDailyQuoteSerializer.java
+++ src/main/java/com/wealthy/machine/bovespa/quote/BovespaDailyQuoteSerializer.java
@@ -3,7 +3,6 @@ package com.wealthy.machine.bovespa.quote;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.ser.std.StdSerializer;
-import com.wealthy.machine.core.sharecode.ShareCode;
 
 import java.io.IOException;
 

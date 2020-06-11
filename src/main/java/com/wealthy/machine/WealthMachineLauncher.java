f2d9816c2c01b85c73b86c9287dc3b38481aab02
luiza <luizaugusto3b@gmail.com>
2020-06-11 18:52:02 +0200

    - fixing the problem with run directory

--- src/main/java/com/wealthy/machine/WealthMachineLauncher.java
+++ src/main/java/com/wealthy/machine/WealthMachineLauncher.java
@@ -6,19 +6,18 @@ import com.wealthy.machine.core.DataUpdater;
 import com.wealthy.machine.core.util.DataFileGetter;
 import com.wealthy.machine.core.util.data.JsonDataFileHandler;
 
-import java.io.IOException;
-import java.nio.file.Files;
+import java.io.File;
 
 public class WealthMachineLauncher {
 
-	public static void main(String... args) throws IOException {
+	public static void main(String... args) {
 		var config = new Config();
 		var logger = config.getLogger(WealthMachineLauncher.class);
 		logger.info("Data updating has started!");
 		var dataUpdaters = new DataUpdater[]{new BovespaDataUpdater()};
 		for (DataUpdater dataUpdater : dataUpdaters) {
 			logger.info("Starting={}", dataUpdater.getClass());
-			var storageFolder = Files.createTempDirectory("storage_folder").toFile();
+			var storageFolder = new File(System.getProperty("user.dir"));
 			var dataFileGetter = new DataFileGetter(storageFolder);
 			var dataFileHandler = new JsonDataFileHandler(dataFileGetter);
 			dataUpdater.execute(dataFileHandler, config);
--- src/main/java/com/wealthy/machine/core/Config.java
+++ src/main/java/com/wealthy/machine/core/Config.java
@@ -10,10 +10,6 @@ public class Config {
 
 	private static final String DEFAULT_BOVESPA_URL;
 	private static final String INITIAL_YEAR;
-	private static final String REPOSITORY_ENV_VAR_NAME ;
-	private static final String GIT_SSH_PUBLIC_KEY_ENV_VAR_NAME;
-	private static final String GIT_SSH_PRIVATE_KEY_ENV_VAR_NAME;
-	private static final String GIT_SSH_PHASSPHRASE_KEY_ENV_VAR_NAME;
 
 	static {
 		var appProps = new Properties();
@@ -26,10 +22,6 @@ public class Config {
 			appProps.load(appConfigPath);
 			DEFAULT_BOVESPA_URL = appProps.getProperty("bovespaUrl", "http://bvmf.bmfbovespa.com.br/InstDados/SerHist/COTAHIST_A{{YYYY}}.ZIP");
 			INITIAL_YEAR = appProps.getProperty("initialYear", "2000");
-			REPOSITORY_ENV_VAR_NAME = appProps.getProperty("repositoryEnvVarName", "DATA_REPOSITORY");
-			GIT_SSH_PRIVATE_KEY_ENV_VAR_NAME = appProps.getProperty("gitSshPrivateKeyEnvName", "GIT_PRIVATE_SSH_KEY");
-			GIT_SSH_PUBLIC_KEY_ENV_VAR_NAME = appProps.getProperty("gitSshPublicKeyEnvName", "GIT_PUBLIC_SSH_KEY");
-			GIT_SSH_PHASSPHRASE_KEY_ENV_VAR_NAME = appProps.getProperty("gitSshPassphraseKeyEnvName", "GIT_PASSPHRASE_SSH_KEY");
 		} catch (IOException e) {
 			var logger = LoggerFactory.getLogger(Config.class);
 			logger.error("Error during loading properties", e);
@@ -48,23 +40,7 @@ public class Config {
 		return DEFAULT_BOVESPA_URL;
 	}
 
-	public Logger getLogger(Class clazz) {
+	public Logger getLogger(Class<?> clazz) {
 		return LoggerFactory.getLogger(clazz);
-	}
-
-	public String getRepositoryPath() {
-		return System.getenv(REPOSITORY_ENV_VAR_NAME);
-	}
-
-	public String getGitSshPublicKey() {
-		return System.getenv(GIT_SSH_PUBLIC_KEY_ENV_VAR_NAME);
-	}
-
-	public String getGitSshPrivateKey() {
-		return System.getenv(GIT_SSH_PRIVATE_KEY_ENV_VAR_NAME);
-	}
-
-	public String getGitSshPassphrase() {
-		return System.getenv(GIT_SSH_PHASSPHRASE_KEY_ENV_VAR_NAME);
 	}
 }
